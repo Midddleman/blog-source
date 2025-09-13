3c5da68adc9e4e90f4753305e0b233fdfecc9547
Midddleman <g2199192503@outlook.com>
2025-09-13 15:13:59 +0900

    Site updated: 2025-09-13 15:13:59

--- js/bing.js
+++ js/bing.js
@@ -1,8 +1,14 @@
-fetch("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN")
-  .then(res => res.json())
-  .then(data => {
-    const imgUrl = "https://www.bing.com" + data.images[0].url;
-    document.body.style.backgroundImage = `url(${imgUrl})`;
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundPosition = "center";
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN")
+    .then(res => res.json())
+    .then(data => {
+      const imgUrl = "https://www.bing.com" + data.images[0].url;
+      console.log("Bing 壁纸 URL:", imgUrl);
+      document.body.style.setProperty(
+        "background",
+        `url(${imgUrl}) center center / cover no-repeat fixed`,
+        "important"
+      );
+    })
+    .catch(err => console.error("获取 Bing 壁纸失败:", err));
+});
