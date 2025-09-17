43c04781fe8f49cf19177f93fecfafc0efaaede2
Midddleman <g2199192503@outlook.com>
2025-09-18 01:31:01 +0900

    修改blogger文字颜色

---
+++ source/js/particles-init.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+  particlesJS("particles-js", {
+    "particles": {
+      "number": { "value": 60 },
+      "size": { "value": 3 },
+      "color": { "value": "#002fa7" }
+    },
+    "interactivity": {
+      "events": {
+        "onhover": { "enable": true, "mode": "repulse" },
+        "onclick": { "enable": true, "mode": "push" }
+      }
+    }
+  });
+});
